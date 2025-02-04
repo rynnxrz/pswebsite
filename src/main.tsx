24df349042ec77fbea2c9150ae4cf8938926d2c2
FanqieUoA <98016410+FanqieUoA@users.noreply.github.com>
2025-02-04 21:00:25 +0000

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+// main.tsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'  // 引入 Tailwind
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
